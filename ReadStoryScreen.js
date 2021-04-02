9ab15e265a4cb80cb7c0f50dcd265b572314c2cc
kabeerWadhwa <68274419+kabeerWadhwa@users.noreply.github.com>
2021-04-02 20:31:22 +0100

    Add files via upload

---
+++ ReadStoryScreen.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import {Text, View, StyleSheet, Image} from 'react-native'
+import { SearchBar } from 'react-native-elements';
+
+export default class App extends React.Component {
+  state = {
+    search: '',
+  };
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+  render() {
+    const { search } = this.state;
+    return (
+      <SearchBar
+        placeholder="Type Here..."
+        onChangeText={this.updateSearch}
+        value={search}
+      />
+    );
+  }
+}
